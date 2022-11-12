5d411ad5e2d8fbbcf7b62b0884ec56438e36472b
Lablinux80 <111852252+lablinux80@users.noreply.github.com>
2022-11-12 21:42:50 +0700

    Update build.sh

--- images/nginx-curl/build.sh
+++ images/nginx-curl/build.sh
@@ -1,11 +1,11 @@
 # Build image
-docker build -t khannedy/nginx-curl .
+docker build -t junius/nginx-curl
 
 # Push image
-docker push khannedy/nginx-curl
+docker push junius/nginx-curl
 
 # Create container
-docker container create --name nginx-curl khannedy/nginx-curl
+docker container create --name nginx-curl junius/nginx-curl
 
 # Start container
 docker container start nginx-curl
